89d5aebafbf7236ef85ccd05aaea794ac3a24668
Xiaohan <zhaoxhmail@163.com>
2022-02-21 20:14:46 -0800

    Assignment04

--- Assignment04/src/com/neu/Assignment04.java
+++ Assignment04/src/com/neu/Assignment04.java
@@ -6,12 +6,22 @@ public class Assignment04 {
     public static void main(String[] args) {
 
     }
-    public static class ListNode{
+
+    public static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val){ this.val = val; }
-        ListNode(int val, ListNode next){ this.val= val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
 
@@ -21,13 +31,13 @@ public class Assignment04 {
         ListNode head1 = list1;
         ListNode cur1 = head1;
         int i = 0;
-        while(i<a-1){
+        while (i < a - 1) {
             cur1 = cur1.next;
             i++;
         }
         ListNode pre = cur1;
         //pre -> a的前一个节点
-        while(i<= b){
+        while (i <= b) {
             cur1 = cur1.next;
             i++;
         }
@@ -35,7 +45,7 @@ public class Assignment04 {
         ListNode head2 = list2;
         ListNode cur2 = head2;
         pre.next = cur2;
-        while(cur2.next != null){
+        while (cur2.next != null) {
             cur2 = cur2.next;
         }
         cur2.next = cur1;
@@ -51,79 +61,132 @@ public class Assignment04 {
         ListNode BH = null;//大于头
         ListNode BT = null;//大于尾
 
-        while(head != null){
+        while (head != null) {
             ListNode next = head.next;
             head.next = null;
-            if(head.val < x){
-                if(SH == null){//是第一个往小于等于区放的
+            if (head.val < x) {
+                if (SH == null) {//是第一个往小于等于区放的
                     SH = head;
                     ST = head;
-                }else{
+                } else {
                     ST.next = head;
                     ST = head;// ST = ST.next
                 }
             }
-            if(head.val >= x){
-                if(BH == null){
+            if (head.val >= x) {
+                if (BH == null) {
                     BH = head;
                     BT = head;
-                }else{
+                } else {
                     BT.next = head;
                     BT = head;
                 }
             }
             head = next;
         }
-        if(ST != null){
-            if(BT != null){
+        if (ST != null) {
+            if (BT != null) {
                 ST.next = BH;
                 return SH;
-            }else{
+            } else {
                 ST.next = null;
                 return SH;
             }
-        }return BH;
+        }
+        return BH;
     }
 
     //Q3 2074. Reverse Nodes in Even Length Groups
+    public ListNode reverseEvenLengthGroups(ListNode head) {
+        ListNode node = head.next;
+        ListNode node_pre = head;
+        ListNode p_pre = head;
+        ListNode p = node;
 
+        int count = 1;
+        while (node != null) {
+            count = count + 1;
+            int a = 0;
+            while (a < count && node != null) {
+                node_pre = node;
+                node = node.next;
+                a++;
+            }
+            if (node != null) {
+                if (a % 2 == 0) {
+                    node_pre.next = null;
+                    p_pre.next = null;
+                    p_pre.next = reverse(p, node);
+                    p.next = node;
+                    p = p.next;
+                    while (p_pre.next != p) {
+                        p_pre = p_pre.next;
+                    }
+                } else {
+                    p = node;
+                    p_pre = node_pre;
+                }
+            } else {
+                if (a % 2 == 0) {
+                    p_pre.next = reverse(p, null);
+                }
+            }
+        }
+        return head;
+    }
+
+    public ListNode reverse(ListNode head, ListNode node1) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+
+        head.next = node1;
+        return pre;
+    }
 
 
     //Q4 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points
 
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int[] res1 = {-1,-1};
-        if(head.next == null || head.next.next == null) return res1;
+        int[] res1 = {-1, -1};
+        if (head.next == null || head.next.next == null) return res1;
         ListNode pre = head;
         ListNode cur = head.next;
         int i = 0;
         List<Integer> list = new ArrayList<>();
-        while(cur.next != null){
+        while (cur.next != null) {
             ListNode nxt = cur.next;
-            if((cur.val > pre.val && cur.val > nxt.val)
-                    ||(cur.val < pre.val && cur.val < nxt.val)){
+            if ((cur.val > pre.val && cur.val > nxt.val)
+                    || (cur.val < pre.val && cur.val < nxt.val)) {
                 list.add(i);
             }
             pre = cur;
             cur = nxt;
             i++;
         }
-        if(list.size() < 2){
+        if (list.size() < 2) {
             return res1;
-        }else if(list.size() == 2){
+        } else if (list.size() == 2) {
             int dis = list.get(1) - list.get(0);
-            int[]res2 = {dis,dis};
+            int[] res2 = {dis, dis};
             return res2;
-        }else{
-            int maxDis = list.get(list.size()-1) - list.get(0);
+        } else {
+            int maxDis = list.get(list.size() - 1) - list.get(0);
             int[] arr = new int[list.size() - 1];
-            for(i = 0 ; i < arr.length; i++){
-                int dis = list.get(i+1) - list.get(i);
+            for (i = 0; i < arr.length; i++) {
+                int dis = list.get(i + 1) - list.get(i);
                 arr[i] = dis;
             }
             Arrays.sort(arr);
             int minDis = arr[0];
-            int[] res = {minDis,maxDis};
+            int[] res = {minDis, maxDis};
             return res;
         }
     }
@@ -132,12 +195,12 @@ public class Assignment04 {
     //Q5 148. Sort List
 
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) return head;
+        if (head == null || head.next == null) return head;
 
         //快慢指针找中点
         ListNode fast = head.next;
         ListNode slow = head;
-        while(fast.next!= null && fast.next.next != null){
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
@@ -145,17 +208,18 @@ public class Assignment04 {
         slow.next = null;//左区断开
         ListNode left = sortList(head);
         ListNode right = sortList(tmp);
-        ListNode res = merge(left,right);
+        ListNode res = merge(left, right);
         return res;
     }
-    public ListNode merge(ListNode left, ListNode right){
+
+    public ListNode merge(ListNode left, ListNode right) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
-        while(left != null && right != null){
-            if(left.val <= right.val){
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
                 cur.next = left;
                 left = left.next;
-            }else{
+            } else {
                 cur.next = right;
                 right = right.next;
             }
@@ -173,14 +237,14 @@ public class Assignment04 {
 
     public Solution(ListNode head) {
         ListNode cur = head;
-        while(cur!= null){
+        while (cur != null) {
             list.add(cur.val);
             cur = cur.next;
         }
     }
 
     public int getRandom() {
-        return list.get((int)(Math.random() * list.size()));
+        return list.get((int) (Math.random() * list.size()));
     }
 
 
@@ -189,12 +253,12 @@ public class Assignment04 {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode fPre = dummy;
-        for (int i = 0; i < left -1 ; i++){
+        for (int i = 0; i < left - 1; i++) {
             fPre = fPre.next;
         }
         ListNode tail = fPre.next;
         ListNode cur = tail.next;
-        for(int i = left; i < right; i++){
+        for (int i = left; i < right; i++) {
             ListNode nxt = cur.next;
             cur.next = fPre.next;
             fPre.next = cur;
@@ -210,21 +274,21 @@ public class Assignment04 {
     public ListNode[] splitListToParts(ListNode head, int k) {
         int len = 0;
         ListNode cur = head;
-        while(cur != null){
+        while (cur != null) {
             len++;
             cur = cur.next;
         }
-        int n = len/k;//每组n个数
-        int a = len%k;//余数
+        int n = len / k;//每组n个数
+        int a = len % k;//余数
 
         // 共k组，前边有a组长度为(n+1)的链表
         ListNode[] res = new ListNode[k];
 
-        if(n != 0){
+        if (n != 0) {
             //前边的a组长度为n+1
-            for(int i = 0; i<a; i++){
+            for (int i = 0; i < a; i++) {
                 cur = head;
-                for(int j = 1; j < n+1;j++){//循环完 cur走到n+1的
+                for (int j = 1; j < n + 1; j++) {//循环完 cur走到n+1的
                     cur = cur.next;
                 }
                 ListNode nxt = cur.next;
@@ -234,9 +298,9 @@ public class Assignment04 {
             }
 
             //剩下的每组n个
-            for(int i = a; i < k; i++){//不是i<k-1
+            for (int i = a; i < k; i++) {//不是i<k-1
                 cur = head;
-                for(int j = 1; j< n; j++){
+                for (int j = 1; j < n; j++) {
                     cur = cur.next;
                 }
                 ListNode nxt = cur.next;
@@ -245,17 +309,17 @@ public class Assignment04 {
                 head = nxt;
             }
             return res;
-        }else{
+        } else {
             int i = 0;
             cur = head;
-            while(cur!= null){
+            while (cur != null) {
                 ListNode tmp = cur.next;
                 cur.next = null;
                 res[i] = cur;
                 cur = tmp;
                 i++;
             }
-            for(i= len;i<k;i++){
+            for (i = len; i < k; i++) {
                 res[i] = null;
             }
             return res;
@@ -267,14 +331,16 @@ public class Assignment04 {
 
     public int numComponents(ListNode head, int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int i:nums){set.add(i);}
+        for (int i : nums) {
+            set.add(i);
+        }
 
         ListNode cur = head;
         int res = 0;
 
-        while(cur != null){
-            if(set.contains(cur.val) &&
-                    (cur.next == null || !set.contains(cur.next.val))){
+        while (cur != null) {
+            if (set.contains(cur.val) &&
+                    (cur.next == null || !set.contains(cur.next.val))) {
                 res++;
             }
             cur = cur.next;
@@ -284,13 +350,13 @@ public class Assignment04 {
 
 
     //Q10 2130. Maximum Twin Sum of a Linked List
-    
+
     public int pairSum(ListNode head) {
-        if(head == null || head.next == null) return 0;
-        if(head.next.next == null) return head.val+head.next.val;
+        if (head == null || head.next == null) return 0;
+        if (head.next.next == null) return head.val + head.next.val;
         ListNode slow = head;
         ListNode fast = head.next;
-        while(fast.next != null && fast.next.next!= null){
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }//slow在中间靠左(0->1->2->3),slow 在1处
@@ -301,8 +367,8 @@ public class Assignment04 {
         ListNode p1 = head;
         ListNode p2 = right;
         int max = 0;
-        while(p1 !=null){
-            max = ((p1.val+p2.val) > max) ? (p1.val+p2.val) : max;
+        while (p1 != null) {
+            max = ((p1.val + p2.val) > max) ? (p1.val + p2.val) : max;
             p1 = p1.next;
             p2 = p2.next;
         }
@@ -318,11 +384,12 @@ public class Assignment04 {
         // // p1.next = right;
         // return tSet.last();
     }
-    public ListNode reverse(ListNode head){
-        if(head == null) return null;
+
+    public ListNode reverse(ListNode head) {
+        if (head == null) return null;
         ListNode cur = head;
         ListNode pre = null;
-        while(cur != null){
+        while (cur != null) {
             ListNode nxt = cur.next;
             cur.next = pre;
             pre = cur;
@@ -330,9 +397,6 @@ public class Assignment04 {
         }
         return pre;
     }
-
-
-
 
 
 }
